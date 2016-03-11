20395534f3d603eff50af2ab60c6f2afee0fc512
mitogh <mitogh@gmail.com>
2016-03-11 17:00:28 -0600

    Update the type of the functio

--- src/Inc/Template.php
+++ src/Inc/Template.php
@@ -8,24 +8,21 @@
 class Template
 {
 	/**
-	 * Returns the template used on a page:
-	 *
-	 * - Default for the default page template
-	 * - Template Slug
-	 * - Empty string if the post is not a page.
+	 * Returns the type for the post, the term template applies to what type of
+	 * post is or what type of template is using.
 	 *
 	 * @param Int|\WP_Post $post The post
 	 * @return string
 	 */
 	public static function get( $post ) {
 		$post = is_a( $post, 'WP_Post' ) ? $post : get_post( $post );
-		$template = '';
-		if ( 'page' === $post->post_type ) {
+		$type = $post->post_type;
+		if ( 'page' === $type ) {
 			$template_slug = get_page_template_slug( $post->ID );
-			$template = empty( $template_slug )
+			$type = empty( $template_slug )
 				? 'default'
 				: basename( $template_slug, '.php' );
 		}
-		return $template;
+		return $type;
 	}
 }
